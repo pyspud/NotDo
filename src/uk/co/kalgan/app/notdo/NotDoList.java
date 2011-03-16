323f0cd1d66bbf7b03185bf749faa29aa487e3b5
Kevin Henderson <pyspud@gmail.com>
2011-03-16 18:20:10 +0000

    Initial Generated Files
    Initial NotDo Android Project files.
    Application will be based on the Example To-do application from
    Professional Android 2 Application Development Book by Reto Meier.

---
+++ src/uk/co/kalgan/app/notdo/NotDoList.java
@@ -0,0 +1,13 @@
+package uk.co.kalgan.app.notdo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotDoList extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

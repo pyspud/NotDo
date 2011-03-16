799694150d79b08d331a4d127b818d97c76801ed
Kevin Henderson <pyspud@gmail.com>
2011-03-16 22:55:54 +0000

    Add UI elements
    Add a EditText and ListView for the UI.
    When Middle Dpad button pressed adds text to list.
    From Chapter2 Page41

--- src/uk/co/kalgan/app/notdo/NotDoList.java
+++ src/uk/co/kalgan/app/notdo/NotDoList.java
@@ -1,13 +1,50 @@
 package uk.co.kalgan.app.notdo;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class NotDoList extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Inflate your View
         setContentView(R.layout.main);
+        
+        // Get references to UI widgets
+        ListView myListView = (ListView)findViewById(R.id.myListView);
+        final EditText myEditText = (EditText)findViewById(R.id.myEditText);
+        
+        // Create the array list of not do items
+        final ArrayList<String> notDoItems = new ArrayList<String>();
+        // Create the array adapter to bind the array to the listview
+        final ArrayAdapter<String> aa;
+        aa = new ArrayAdapter<String>(this,
+        		android.R.layout.simple_list_item_1,
+        		notDoItems);
+        // Bind the array adapter to the listview
+        myListView.setAdapter(aa);
+        
+        myEditText.setOnKeyListener(new OnKeyListener() {
+        	public boolean onKey(View v, int keyCode, KeyEvent event) {
+        		if (event.getAction() == KeyEvent.ACTION_DOWN)
+        			if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER){
+        				notDoItems.add(0, myEditText.getText().toString());
+        				aa.notifyDataSetChanged();
+        				myEditText.setText("");
+        				return true;
+        			}
+        		return false;
+        	}
+        });
     }
 }
